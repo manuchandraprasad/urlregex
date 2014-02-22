86aa88ff81d3446cf6fab69a2e2373e8442f667f
manuchandraprasad <manu@quadloops.com>
2014-02-22 17:52:10 +0530

    Docstrings updated with logic of function

--- url_regex.py
+++ url_regex.py
@@ -16,6 +16,9 @@ def long_substr(data):
 
 
 def substring_pos(string,substring):
+    """
+    Find the position of the substring in the given string and substitute ^ at boundaries
+    """
     pos = string.index(substring)
     if pos == 0:
         #Substring is in the begining of string
@@ -26,16 +29,12 @@ def substring_pos(string,substring):
     else:
         return "^"+substring+"^"
 
-"""
-Idea is to find the only changing part from the URL and make a regular expression out of it. 
-
-1.First find the longest common substring prefix
-2.The remove it from the urls and obtain a list of the remaining URL
-3.Repeat one till there is no more LCS 
-4.Make pattern from the list LCSs
-"""
 
 def find_common_substrs(data):
+    """
+    Find the list of longest common substrings in a given list of URLs (strings) 
+    Returns a list of strings with ^ substituted at the boundaries
+    """
     longest_common_substrings = []
     while(long_substr(data)):
         lcs = long_substr(data)
@@ -48,6 +47,10 @@ def find_common_substrs(data):
     return longest_common_substrings
 
 def create_regex(data):
+    """
+    Replaces the ^ character to add '.+' regex 
+    """
+    #FIXME: Improve code to find the data type of the variable part and use a regex rule to replace it 
     s = ("".join(find_common_substrs(data)))
     s = re.escape(s) 
     s = s.replace('\^','^')
