cbbdadc7b295775ce26049dd9c8bc52f9ab37c73
manuchandraprasad <manu@quadloops.com>
2014-02-22 01:13:31 -0800

    Function to find longest substring from list of strings

---
+++ url_regex.py
@@ -0,0 +1,13 @@
+def long_substr(data):
+	"""
+	Prints the longest string that is common in the list of strings provided
+	Source: http://stackoverflow.com/questions/2892931/longest-common-substring-from-more-than-two-strings-python
+	"""
+
+    substr = ''
+    if len(data) > 1 and len(data[0]) > 0:
+        for i in range(len(data[0])):
+            for j in range(len(data[0])-i+1):
+                if j > len(substr) and all(data[0][i:i+j] in x for x in data):
+                    substr = data[0][i:i+j]
+    return substr
